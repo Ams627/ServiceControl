faa7d4edcc3dbca34f7e7173cc5b54283e875480
Adrian Sims <adriansims62781@gmail.com>
2020-04-02 14:37:55 +0200

    Updates

--- ServiceControl/Program.cs
+++ ServiceControl/Program.cs
@@ -2,22 +2,28 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.ServiceProcess;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
 using Microsoft.Win32;
+using System.ServiceProcess;
 
 namespace ServiceControl
 {
     class Program
     {
+
         private static void Main(string[] args)
         {
             try
             {
-                PrintServicesFromRegistry();
+
+//                PrintServicesFromRegistry();
                 ServiceController service = new ServiceController("WpnService");
                 Console.WriteLine($"service status is {service.Status}");
+
+                var ws = new WindowsService();
+                ws.RemoveService("helloservice");
+
             }
             catch (Exception ex)
             {
@@ -28,6 +34,33 @@ namespace ServiceControl
 
         }
 
+
+        class Finder
+        {
+            private readonly string pattern;
+            private readonly string startdir;
+            private readonly List<string> files = new List<string>();
+
+            public Finder(string pattern, string startdir = "")
+            {
+                this.pattern = pattern;
+                this.startdir = startdir;
+                FindFiles(pattern, string.IsNullOrEmpty(startdir) ? Directory.GetCurrentDirectory() : startdir);
+            }
+
+            private void FindFiles(string pattern, string startdir)
+            {
+                var files = Directory.GetFiles(startdir, pattern);
+                this.files.AddRange(files);
+                var dirs = Directory.GetDirectories(startdir);
+                foreach (var dir in dirs)
+                {
+                    FindFiles(pattern, dir);
+                }
+            }
+
+            public List<string> Files => files;
+        }
 
 
         private static void PrintServicesFromRegistry(bool is64Bit = true)
--- ServiceControl/WindowsService.cs
+++ ServiceControl/WindowsService.cs
@@ -1,7 +1,58 @@
-﻿namespace ServiceControl
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Win32;
+
+namespace ServiceControl
 {
     class WindowsService
     {
+        public void AddService(string name)
+        {
+            var keyName = @"SYSTEM\CurrentControlSet\Services";
+            var registryView = RegistryView.Registry64;
+            using (var basekey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, registryView))
+            {
+                using (var key = basekey.OpenSubKey(keyName, true))
+                {
+                    var subkeyNames = key.GetSubKeyNames();
+                    if (subkeyNames == null)
+                    {
+                        throw new Exception($"Cannot get subkeys from {keyName}");
+                    }
+                    var keyset = new HashSet<string>(subkeyNames);
+                    if (keyset.Contains(name))
+                    {
+                        throw new Exception($"the registry entry for the service {key} already exists");
+                    }
+                    using (var newkey = key.CreateSubKey(name))
+                    {
 
+                    }
+                }
+            }
+        }
+
+        public void RemoveService(string name)
+        {
+            var keyName = @"SYSTEM\CurrentControlSet\Services";
+            using (var basekey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64))
+            {
+                using (var key = basekey.OpenSubKey(keyName, true))
+                {
+                    var subkeyNames = key.GetSubKeyNames();
+                    if (subkeyNames == null)
+                    {
+                        throw new Exception($"Cannot get subkeys from {keyName}");
+                    }
+                    var keyset = new HashSet<string>(subkeyNames);
+                    if (!keyset.Contains(name))
+                    {
+                        throw new Exception($"the registry entry for the service {key} does not exist");
+                    }
+
+                    key.DeleteSubKeyTree(name);
+                }
+            }
+        }
     }
 }
